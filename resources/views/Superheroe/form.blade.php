ad5c4b9478ffb8886387f660ed183c060718049b
Patricio <patoestradac@gmail.com>
2023-03-26 00:51:02 -0500

    Interfáz grafica añadida a Index y a Formulario

--- resources/views/Superheroe/form.blade.php
+++ resources/views/Superheroe/form.blade.php
@@ -1,25 +1,34 @@
-<label for="NombreReal">Nombre Real</label>   
-<input type="text" name="NombreReal" id="NombreReal" value="{{isset($superheroe->NombreReal)?$superheroe->NombreReal:''}}">
-<br>
+<div class="form-group">
+    <label for="NombreReal">Nombre Real</label>   
+    <input type="text" name="NombreReal" id="NombreReal" value="{{isset($superheroe->NombreReal)?$superheroe->NombreReal:''}}" class="form-control">
+    <br>
+</div>
 
-<label for="NombreSuper">Nombre de Heroe</label>   
-<input type="text" name="NombreSuper" id="NombreSuper" value="{{isset($superheroe->NombreSuper)?$superheroe->NombreSuper:''}}">
-<br>
+<div class="form-group">
+    <label for="NombreSuper">Nombre de Heroe</label>   
+    <input type="text" name="NombreSuper" id="NombreSuper" value="{{isset($superheroe->NombreSuper)?$superheroe->NombreSuper:''}}" class="form-control">
+    <br>
+</div>
 
-<label for="FotoURL">Archivo foto</label>   
+<div class="form-group">
+    <label for="FotoURL">Foto: </label>   
+    @if(isset($superheroe->FotoURL))
+        <img src="{{asset('storage').'/'.$superheroe->FotoURL}}" width="100" alt="" class="img-thumbnail">
+    @endif
 
-@if(isset($superheroe->FotoURL))
-    <img src="{{asset('storage').'/'.$superheroe->FotoURL}}" width="100" alt="">
-@endif
+    <input type="file" name="FotoURL" id="FotoURL" class="form-control">
+    <br>
+</div>
 
-<input type="file" name="FotoURL" id="FotoURL">
-<br>
-
-<label for="InfoExtra">Informacion extra</label>   
-<input type="text" name="InfoExtra" id="InfoExtra" value="{{isset($superheroe->InfoExtra)?$superheroe->InfoExtra:''}}">
-<br>
+<div class="form-group">
+    <label for="InfoExtra">Informacion extra</label>   
+    <input type="text" name="InfoExtra" id="InfoExtra" value="{{isset($superheroe->InfoExtra)?$superheroe->InfoExtra:''}}" class="form-control">
+    <br>
+</div>
     
-<input type="submit" value="{{$modo}}">
-<br><br><br>
+<div class="form-group">
+    <input type="submit" value="{{$modo}}" class="form-control btn btn-success">
+    <br><br><br><br>
+</div>
 
-<a href="{{url('Superheroe')}}">Regresar</a>
+<a href="{{url('Superheroe')}}" class="btn btn-primary">Regresar</a>
--- resources/views/Superheroe/index.blade.php
+++ resources/views/Superheroe/index.blade.php
@@ -2,9 +2,9 @@
 @section('content')
 <div class="container">
 
-    <a href="{{url('Superheroe/create')}}">Registrar a un nuevo heroe</a><br><br>
+    <h1>Lista de Superheroes guardados</h1><br>
 
-    <h1>Lista de Superheroes guardados</h1>
+    <a href="{{url('Superheroe/create')}}" class="btn btn-success btn-lg">Registrar a un nuevo heroe</a><br><br>
 
     <h4>
 
@@ -33,21 +33,21 @@
             <tr>
                 <td>{{$superheroe->id}}</td>
                 <td>
-                    <img src="{{asset('storage').'/'.$superheroe->FotoURL}}" width="100" alt="">
+                    <img src="{{asset('storage').'/'.$superheroe->FotoURL}}" width="100" alt="" class="img-thumbnail">
                 </td>
                 <td>{{$superheroe->NombreReal}}</td>
                 <td>{{$superheroe->NombreSuper}}</td>
                 <td>{{$superheroe->InfoExtra}}</td>
                 <td>
                     
-                    <a href="{{url('/Superheroe/'.$superheroe->id.'/edit')}}">
+                    <a href="{{url('/Superheroe/'.$superheroe->id.'/edit')}}" class="btn btn-info">
                         Editar
                     </a>
                     
-                    <form action="{{url('/Superheroe/'.$superheroe->id)}}"  method="post">
+                    <form action="{{url('/Superheroe/'.$superheroe->id)}}"  method="post" class="d-inline">
                         @csrf
                         {{method_field('DELETE')}}
-                        <input type="submit" onclick="return confirm('¿Quieres eliminar este registro? Se perdera para siempre')" value="Eliminar"/>
+                        <input type="submit" onclick="return confirm('¿Quieres eliminar este registro? Se perdera para siempre')" value="Eliminar" class= "btn btn-danger"/>
                     </form>
 
                 </td>
